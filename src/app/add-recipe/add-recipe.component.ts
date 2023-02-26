f0ff2ca09722bf204f79b41f7939aebc7b4d8a67
Warren A Stowe <stowewa@gmail.com>
2023-02-26 09:12:16 -0500

    add recipe request to backend successful with dummy data

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -10,10 +10,86 @@ import { RecipeService } from '../services/recipe.service';
 export class AddRecipeComponent {
 
   constructor(private recipeService: RecipeService) {  }
+  
 
   onSubmit() {
-    
-    let response = this.recipeService.callTest().subscribe();
+
+    let request = {
+      recipe: {
+        recipeName: 'Hamburgers2',
+        source: 'Warren',
+        sourcePage: '0'
+      },
+      ingredientQuantities: [
+          
+          {
+              ingredient: {
+                  id: 3,
+                  ingredient: "ketchup",
+                  aisle: "condiments"
+              },
+              quantity: {
+                  amount: 1,
+                  measurement: "units",
+                  optional: false
+              }
+          }          ,
+          {
+              ingredient: {
+                  id: 4
+              },
+              quantity: {
+                  amount: 1,
+                  measurement: "units",
+                  optional: false
+              }
+          },
+          {
+              ingredient: {
+                  id: 5
+              },
+              quantity: {
+                  amount: 1,
+                  measurement: "units",
+                  optional: true
+              }
+          },{
+              ingredient: {
+                  id: 7
+              },
+              quantity: {
+                  amount: 8,
+                  measurement: "units",
+                  optional: false
+              }
+          },
+          {
+              ingredient: {
+                  id: 8
+              },
+              quantity: {
+                  amount: 8,
+                  measurement: "units",
+                  optional: false
+              }
+          },
+          {
+            ingredient: {
+              id: 0,
+              ingredientName: "Pineapple",
+              aisle: "Produce"
+            },
+            quantity: {
+              amount: 1,
+              measurement: "units",
+              optional: true
+            }
+          }
+      ]
+    }
+
+    // let addRecipeRequest = JSON.stringify(request);
+    let response = this.recipeService.addRecipe(request).subscribe();
     console.log("In component: ");
     console.log(response);
   }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -11,17 +11,24 @@ export class RecipeService {
   baseUrl: string;
 
   constructor(private http: HttpClient) { 
-    this.baseUrl = 'http://localhost:8080/recipes/test';
+    this.baseUrl = 'http://localhost:8080/recipes/';
   }
 
   public callTest() {
     return this.http.get<string>(this.baseUrl);
   }  
 
+
+
   getRecipesByName(recipeName: string): Observable<Recipe[]> {
-    let response = this.http.get<Recipe[]>("http://localhost:8080/recipes/search/" + recipeName);
+    let response = this.http.get<Recipe[]>("search/" + recipeName);
     console.log("In Service: ");
     console.log(response);
     return response;
   }  
+
+  addRecipe(addRecipeRequest: any) {
+    let response = this.http.post<string>(this.baseUrl + "add", addRecipeRequest);
+    return response;
+  }
 }
